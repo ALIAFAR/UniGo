542e55093fc307e302af9214de6451c575a344d4
ALIAFAR <alifarvazova@gmail.com>
2025-06-09 14:42:46 +0500

    если че тоже отмена

--- server/routes/tripRouter.js
+++ server/routes/tripRouter.js
@@ -8,7 +8,7 @@ router.post('/trip',authMiddleware,tripController.create)//create trip
 router.get('/search',authMiddleware,tripController.get_driver_trips)//get all trip
 router.get('/check-diver',authMiddleware,tripController.checkDriverStatus)//get all trip
 router.get('/searchResult',tripController.search_result)//get all trip
-router.put('/cancell/:id',authMiddleware,bookingController.cancell_trip)//get all trip
+router.put('/cancell/:id',authMiddleware,tripController.cancell_trip)//get all trip
 //router.get('/:id',tripController.getOne)//get all trip
 
 module.exports = router
